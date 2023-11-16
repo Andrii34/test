fa5fc1d529355a11e82cdf4e054a661449f364d7
Andrii34 <kolesnikandrej330@gmail.com>
2023-11-16 12:34:00 +0100

    init

---
+++ Assets/Fantasy Cyberpunk UI Pack/Cyberpunk UI Resources/Scripts/PlayAudio.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAudio : MonoBehaviour
+{
+    [SerializeField] private AudioSource ass;
+  
+
+    
+    public void Play()
+    {
+        ass.Play();
+    }
+}
